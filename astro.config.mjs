6efa91b64a04e26d28ddaebd499040214adfbaeb
William GM <wm@odden.mx>
2025-04-29 18:25:10 -0600

    set host to true

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({ vite: { plugins: [tailwindcss()] } });
+export default defineConfig({
+  vite: { plugins: [tailwindcss()] },
+  server: { host: true },
+});
